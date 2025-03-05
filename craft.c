4d477a6e8a3a70a339f1d6a858d5590f1bd4cf85
stumburs <stumburs@inbox.lv>
2025-03-05 16:01:19 +0200

    Assert (dominance)

--- craft.c
+++ craft.c
@@ -1,10 +1,11 @@
 #define CRAFT_IMPLEMENTATION
 #include "craft.h"
+#include <assert.h>
 
 int main(void)
 {
-    craft_mkdir_if_doesnt_exist("build");
-    craft_run_cmd("cc ./tests/hello.c -o ./build/hello");
-    craft_run_cmd("./build/hello");
+    assert(craft_mkdir_if_doesnt_exist("build") == true);
+    assert(craft_run_cmd("cc ./tests/hello.c -o ./build/hello") == true);
+    assert(craft_run_cmd("./build/hello") == true);
     return 0;
 }
